71404f8ceff1976aab026a409f1265a7720488dc
ZEHRA <z.nuralkan@gmail.com>
2021-10-19 14:15:36 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("merhaba kodlama.io")
+
+let dolarBugun= 9.30
+let dolarDun=9.20
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun=11 //hata alıyoruz
+console.log(euroDun)
+
+//array
+//camelCasing javascriptte kullanıyoruz.
+//PascalCasing:Baş harfleri buyuk
+let konutKredileri=[ "Konut Kredisi","Emlak konut kredisi","Kamu konut kredisi"]
+
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
